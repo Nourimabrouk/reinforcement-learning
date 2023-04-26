b5a48199283e4e17fe8263ea416f3f0fd4efd1f0
Nouri <46678756+NM@users.noreply.github.com>
2023-04-26 17:21:59 +0200

    .

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import os
 import sys
 import gymnasium as gym
 
-# add src directory to path
-
-from src.utils import demonstrations
-
 def main():
-    print("Starting")
-    demonstrations.selector()
+    env = gym.make("LunarLander-v2", render_mode="human")
+    observation, info = env.reset()
+
+    for _ in range(100):
+        action = env.action_space.sample() 
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
     return
 
 if __name__ == "__main__":
